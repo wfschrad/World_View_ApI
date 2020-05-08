5902cdf286b53a676a286b601f595c6f10a4e0cd
William Schrader <wfschrad@gmail.com>
2020-05-08 16:41:41 -0700

    Add initial documentation.

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+**MVPs**
+
+* Persistent userAuth with attached saved/starred stories
+* Search news based on country/region, topic, popularity, and possibly date
+* Article Cards will respond to click by rendering full article with details
+
+**Components**
+
+* NavBar to show login state and provide basic navigation options
+* Search form with various options for filtering potential news stories
+* Search result container
+* Story Card to contain single search result
+* Detail pane to render entire article
+* Saved/starred stories to house favorite articles saved
---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors');
+
+const { environment } = require('./config');
+
+//routers
+const indexRouter = require('./routes/index');
+const businessesRouter = require('./routes/businesses');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors({ origin: true }));//change path to host-address when acquired
+
+
+//mount routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/businesses', businessesRouter);
+
+
+
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.status = 404;
+    next(err);
+});
+
+// Custom error handlers.
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = environment === "production";
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
+
+module.exports = app;
