f168f3995dd0e4044ae306dc7b761c988cc00dce
William Schrader <wfschrad@gmail.com>
2020-05-09 00:52:46 -0700

    Add postgresql User model.

---
+++ migrations/20200509074330-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING(40),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
