605c25b63a2aa35968577f12d22689d7c260b843
William Schrader <wfschrad@gmail.com>
2020-05-11 11:11:25 -0700

    Add password validation method to user instance.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING(40),
@@ -7,5 +9,10 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function (models) {
     // associations can be defined here
   };
+
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPass.toString());
+  };
+
   return User;
 };
